d21b7158b0a6d6452c3d33eedd74431c2f85ffae
Darla the Cat <pettitsounds@gmail.com>
2024-08-02 20:34:49 +0000

    my first commit

---
+++ website/scripts/main.js
@@ -0,0 +1,11 @@
+var email = prompt("Enter your email");
+function please check your email(){
+if (email.length<1){
+    alert("Email can not be blank.Try again!");
+}else { 
+    alert("Thanks! We'll spam you at" + email);
+}
+} 
+
+
+// please check your email ();
